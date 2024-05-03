394178f3fe7c3081986c9ba301842c34a2245784
Dekan <dekanbrown@odyssy.io>
2024-05-03 15:44:12 -0600

    field clean up

--- src/legos/fields.ts
+++ src/legos/fields.ts
@@ -1,13 +1,7 @@
 import { CustomFieldLego } from "./fieldConfig";
 
 export const APP_FIELD: Record<string, CustomFieldLego> = {
-  NFT_COLLECTION: {
-    id: "nftContractAddress",
-    type: "nftAddress",
-    label: "NFT Contract Address",
-    placeholder: "0x0000...0000",
-    expectType: "ethAddress",
-  },
+
   MANAGER_ACCOUNT: {
     id: "managerAccountAddress",
     type: "managerAddress",
@@ -15,10 +9,7 @@ export const APP_FIELD: Record<string, CustomFieldLego> = {
     placeholder: "0x0000...0000",
     expectType: "ethAddress",
   },
-  AMOUNT_PER_NFT: {
-    id: "amountPerNft",
-    type: "amountPerNft",
-  },
+
   SPACER_FIELD: {
     id: "spacerField",
     type: "spacerField",
