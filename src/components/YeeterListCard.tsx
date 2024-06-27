0be77d4e76e0a70ca9c45fe4aa448a904c68fa4f
skuhlmann <samkuhlmann@gmail.com>
2024-06-27 13:08:55 -0600

    good exit

--- src/components/YeeterListCard.tsx
+++ src/components/YeeterListCard.tsx
@@ -203,7 +203,7 @@ export const YeeterListCard = ({ yeeterData }: { yeeterData: YeeterItem }) => {
         <div className="detailsLink">
           <LinkButton to={`molochv3/${chainId}/${yeeter.dao.id}/${yeeter.id}`}>
             <Button size="sm" style={{ marginTop: "2rem" }} variant="outline">
-              What is this?
+              WHAT IS THIS?
             </Button>
           </LinkButton>
         </div>
