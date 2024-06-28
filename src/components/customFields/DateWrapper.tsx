e2c6124ff570346d98e31c9c0c357257ddc16b03
Dekan <dekanbrown@odyssy.io>
2024-06-28 08:15:31 -0600

    workaround date field errors

--- src/components/customFields/DateWrapper.tsx
+++ src/components/customFields/DateWrapper.tsx
@@ -1,22 +1,48 @@
-import React, { useEffect, useState } from "react";
-import { Buildable, ErrorText, Field, ParSm, } from "@daohaus/ui";
+import React, { forwardRef, useEffect, useState } from "react";
+import { Buildable, Button, ErrorText, Field, ParLg, ParSm, } from "@daohaus/ui";
 
-import { EpochDatePicker } from "@daohaus/moloch-v3-fields";
 import { useFormContext } from "react-hook-form";
 import styled from "styled-components";
+import { formatDate } from "../YeetComments";
+import DatePicker from "react-datepicker";
+import { RiArrowDropDownFill, RiCalendar2Fill } from 'react-icons/ri';
 
 
 
 export const DateWrapper = (props: Buildable<Field>) => {
     const { setValue, watch } = useFormContext();
     const [errorText, setErrorText] = useState<string>("");
+    const [startDate, setStartDate] = useState<Date>(new Date());
 
     const selectedDate = watch(props.id);
+
+    const handleChange = (date: Date) => {
+        if (date) {
+            setStartDate(date);
+            setValue(props.id, Math.floor(+date / 1000));
+        }
+    };
+
+    interface Props {
+        onClick?: () => void;
+        value?: string | number;
+    }
+    const CustomInput = forwardRef<HTMLButtonElement, Props>(
+        ({ value, onClick }, ref) => (
+            <Button
+                IconLeft={RiCalendar2Fill}
+                IconRight={RiArrowDropDownFill}
+                className="custom-button-input"
+                onClick={onClick}
+                ref={ref}
+            >
+            </Button>
+        )
+    );
 
     useEffect(() => {
         setValue(props.id, Math.floor(+(new Date()) / 1000));
     }, []);
-
 
     useEffect(() => {
         console.log("selectedDate", selectedDate, Math.floor(+(new Date()) / 1000), selectedDate < Math.floor(+(new Date()) / 1000));
@@ -28,15 +54,30 @@ export const DateWrapper = (props: Buildable<Field>) => {
     }, [selectedDate]);
 
     const DateWrapperStyle = styled.div`
+        display: flex;
+        flex-direction: row;
+        gap: 8px;
         border: 2px solid ${errorText ? ({ theme }) => theme.warning.step5 : "transparent"};
         padding: 8px;
     `;
 
-  return (
-    <DateWrapperStyle >
-    <EpochDatePicker id={props.id} />
-    {errorText && <ErrorText>{errorText}</ErrorText>}
-    </DateWrapperStyle>
-
-  );
+    return (
+        <>
+        <DateWrapperStyle >
+            <DatePicker
+                id={props.id}
+                selected={startDate}
+                // @ts-ignore
+                onChange={(date: Date) => handleChange(date)}
+                showTimeSelect
+                customInput={<CustomInput />}
+                wrapperClassName={props?.className}
+                dateFormat="Pp"
+            />
+            <ParLg>{formatDate(selectedDate)}</ParLg>
+            
+        </DateWrapperStyle>
+        {errorText && <ErrorText>{errorText}</ErrorText>}
+        </>
+    );
 };
